8e8059a18009c2045a08af5fe65a58150c32d46b
Dale Dahlenburg <dale.dahlenburg@orangesky.org.au>
2024-07-19 15:41:39 +1000

    adding further descriptions

--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ff5accd903743d1250a3a128e631ab6a9ed0d21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c997a3bce8b2390b7144a330bc3506a9c6d156")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
